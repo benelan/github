47387125a64f6fb09fab2caf4a7240f7323cdf66
Ben Elan <no-reply@benelan.dev>
2024-07-16 19:23:39 -0700

    ci: add inputs for notification cleanup time range

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -1,73 +1,89 @@
 // @ts-check
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ github, context }) => {
-  /**
-   * @param {number} days
-   */
-  function date(days) {
-    return new Date(
-      new Date().getTime() - (days ?? 1) * 24 * 60 * 60 * 1000,
-    ).toISOString();
-  }
+  try {
+    /**
+     * @param {number} days
+     */
+    function date(days) {
+      return new Date(
+        new Date().getTime() - (days ?? 1) * 24 * 60 * 60 * 1000,
+      ).toISOString();
+    }
 
-  const before = context.eventName == "workflow_dispatch" ? undefined : date(1);
-  const since = date(3);
+    const since = process.env.SINCE ?? date(3);
+    const before =
+      context.eventName == "workflow_dispatch" ? process.env.BEFORE : date(1);
 
-  console.log(`🧹 Cleaning up notifications`);
-  console.log(`📡 Event name: ${context.eventName}`);
-  console.log(`📅 Current date: ${new Date().toISOString()}`);
-  console.log(`📅 Since: ${since}`);
-  console.log(`📅 Before: ${before}`);
+    console.log(`Cleaning up notifications`);
+    console.log(`Event name: ${context.eventName}`);
+    console.log(`Current date: ${new Date().toISOString()}`);
+    console.log(`Since: ${since}`);
+    console.log(`Before: ${before}`);
 
-  // Fetch notifications for the current page
-  const notifs = await github.paginate("GET /notifications", {
-    all: true,
-    before,
-    since,
-  });
+    // Fetch notifications for the current page
+    const notifs = await github.paginate("GET /notifications", {
+      all: true,
+      before,
+      since,
+    });
 
-  // Loop through each notification and its corresponding ID
-  for (const notif of notifs) {
-    let done = false;
+    // Loop through each notification and its corresponding ID
+    for (const notif of notifs) {
+      let done = false;
 
-    if (
-      // Skip discussions, check suites and releases
-      notif.subject.type == "Discussion" ||
-      notif.subject.type == "CheckSuite" ||
-      notif.subject.type == "Release"
-    ) {
-      // Mark discussions as done
-      done = true;
-    } else if (
-      notif.subject.type == "Issue" ||
-      notif.subject.type == "PullRequest"
-    ) {
-      const latestCommentUrl = notif.subject.latest_comment_url;
-      const details = await github.request(`GET ${notif.subject.url}`);
-      // Mark as done if the issue/PR is closed
-      if (details.data.state === "closed") done = true;
-      // If the issue/PR is open, check if the latest comment is from stale-bot
-      else if (latestCommentUrl) {
-        // Fetch the comment details
-        const comment = await github.request(`GET ${latestCommentUrl}`);
-        done =
-          comment.data.user.login === "github-actions[bot]" &&
-          /stale/.test(comment.data.body);
+      // Skip discussions, check suites, and releases
+      if (
+        ["Discussion", "CheckSuite", "Release"].includes(notif.subject.type)
+      ) {
+        done = true;
+      } else if (["Issue", "PullRequest"].includes(notif.subject.type)) {
+        const details = await github.request(`GET ${notif.subject.url}`);
+
+        // Mark as done if the issue/PR is closed
+        if (details.data.state === "closed") {
+          done = true;
+
+          // If the issue/PR is open, check if the latest comment is from a bot
+        } else if (notif.subject.latest_comment_url) {
+          // Fetch the comment details
+          const comment = await github.request(
+            `GET ${notif.subject.latest_comment_url}`,
+          );
+
+          done =
+            [
+              "github-actions[bot]",
+              "renovate[bot]",
+              "dependabot[bot]",
+            ].includes(comment.data.user.login) &&
+            /(stale|Renovate Ignore|:robot: Release is at|Superseded by)/.test(
+              comment.data.body,
+            );
+        }
+      }
+
+      // remove api. and repos/ from the url
+      const url = (notif.subject.url ?? notif.url)
+        .replace("api.", "")
+        .replace("repos/", "");
+
+      if (done) {
+        console.log(
+          `🧹 [${notif.repository.full_name}] ${notif.subject.title}\n  - ${url}\n`,
+        );
+        await github.request(`DELETE /notifications/threads/${notif.id}`, {
+          thread_id: notif.id,
+          headers: { "X-GitHub-Api-Version": "2022-11-28" },
+        });
+      } else {
+        console.log(
+          `👀 [${notif.repository.full_name}] ${notif.subject.title}\n  - ${url}\n`,
+        );
       }
     }
-    // remove api. and repos/ from the url
-    const url = (notif.subject.url ?? notif.url)
-      .replace("api.", "")
-      .replace("repos/", "");
-    if (done) {
-      console.log(
-        `❌ [${notif.repository.full_name}] ${notif.subject.title}\n  - ${url}`,
-      );
-      await github.request(`DELETE /notifications/threads/${notif.id}`);
-    } else {
-      console.log(
-        `✅ [${notif.repository.full_name}] ${notif.subject.title}\n  - ${url}`,
-      );
-    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
   }
 };
