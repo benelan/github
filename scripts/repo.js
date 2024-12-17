09a6eafd8137ee86affd6701222c085fa5794ad1
Ben Elan <no-reply@benelan.dev>
2024-12-17 10:55:20 -0800

    ci(repo): remove advanced security settings

--- scripts/repo.js
+++ scripts/repo.js
@@ -39,11 +39,6 @@ module.exports = async ({ github, context }) => {
       allow_squash_merge: true,
       squash_merge_commit_title: "PR_TITLE",
       squash_merge_commit_message: "PR_BODY",
-      security_and_analysis: {
-        advanced_security: { status: "enabled" },
-        secret_scanning: { status: "enabled" },
-        secret_scanning_push_protection: { status: "enabled" },
-      },
     });
 
     // allow the default GITHUB_TOKEN to create and approve pull requests
