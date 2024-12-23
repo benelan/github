a06122c33e46f236a9445414a0119546e57933af
Ben Elan <no-reply@benelan.dev>
2024-12-23 13:00:27 -0800

    chore: allow branch protections

--- scripts/repo.js
+++ scripts/repo.js
@@ -7,16 +7,6 @@ module.exports = async ({ github, context }) => {
     per_page: 100,
   });
 
-  // const LABELS = [
-  //   { name: "bug", color: "#d73a4a" },
-  //   { name: "chore", color: "#f3c69b" },
-  //   { name: "documentation", color: "#0075ca" },
-  //   { name: "enhancement", color: "#a2eeef" },
-  //   { name: "performance", color: "#B822F7" },
-  //   { name: "testing", color: "#fbca04" },
-  //   { name: "autorelease: pending", color: "#167618" },
-  // ];
-
   for (const repo of repos) {
     if (repo.fork || repo.archived || repo.disabled) {
       continue;
@@ -26,11 +16,12 @@ module.exports = async ({ github, context }) => {
     const name = repo.name;
 
     console.log(`Updating ${repo.full_name}...`);
+
     // Enable repository settings
     await github.rest.repos.update({
       repo: name,
       owner: owner,
-      has_discussions: false,
+      // has_discussions: false,
       has_wiki: true,
       allow_update_branch: true,
       delete_branch_on_merge: true,
@@ -50,10 +41,9 @@ module.exports = async ({ github, context }) => {
       },
     );
 
-    const branch = repo.default_branch;
     // Update branch protection
     await github.request(
-      `PUT /repos/${owner}/${name}/branches/${branch}/protection`,
+      `PUT /repos/${owner}/${name}/branches/${repo.default_branch}/protection`,
       {
         headers: {
           accept: "application/vnd.github.v3+json",
@@ -67,46 +57,56 @@ module.exports = async ({ github, context }) => {
         required_conversation_resolution: false,
         required_linear_history: true,
         required_signatures: false,
-        required_status_checks: null,
-        required_pull_request_reviews: null,
-        restrictions: null,
+        // required_status_checks: null,
+        // required_pull_request_reviews: null,
+        // restrictions: null,
       },
     );
 
-    //   // Labels
-    //   const labels = await github.rest.issues.listLabelsForRepo({
-    //     owner,
-    //     repo: name,
-    //   });
+    // const LABELS = [
+    //   { name: "bug", color: "#d73a4a" },
+    //   { name: "chore", color: "#f3c69b" },
+    //   { name: "documentation", color: "#0075ca" },
+    //   { name: "enhancement", color: "#a2eeef" },
+    //   { name: "performance", color: "#B822F7" },
+    //   { name: "testing", color: "#fbca04" },
+    //   { name: "autorelease: pending", color: "#167618" },
+    // ];
     //
-    //   for (const label of LABELS) {
-    //     const existing = labels.data.find(
-    //       (l) => l.name.toLowerCase() === label.name.toLowerCase(),
-    //     );
+    // // Labels
+    // const labels = await github.rest.issues.listLabelsForRepo({
+    //   owner,
+    //   repo: name,
+    // });
     //
-    //     if (
-    //       existing?.color === label.color &&
-    //       existing?.description === label.description
-    //     ) {
-    //       continue;
-    //     }
+    // for (const label of LABELS) {
+    //   const existing = labels.data.find(
+    //     (l) => l.name.toLowerCase() === label.name.toLowerCase(),
+    //   );
     //
-    //     if (existing) {
-    //       await github.rest.issues.updateLabel({
-    //         owner,
-    //         repo: name,
-    //         name: label.name,
-    //         new_name: label.name,
-    //         color: label.color,
-    //       });
-    //     } else {
-    //       await github.rest.issues.createLabel({
-    //         owner,
-    //         repo: name,
-    //         name: label.name,
-    //         color: label.color,
-    //       });
-    //     }
+    //   if (
+    //     existing?.color === label.color &&
+    //     existing?.description === label.description
+    //   ) {
+    //     continue;
     //   }
+    //
+    //   if (existing) {
+    //     await github.rest.issues.updateLabel({
+    //       owner,
+    //       repo: name,
+    //       name: label.name,
+    //       new_name: label.name,
+    //       color: label.color,
+    //     });
+    //   } else {
+    //     await github.rest.issues.createLabel({
+    //       owner,
+    //       repo: name,
+    //       name: label.name,
+    //       color: label.color,
+    //     });
+    //   }
+    // }
   }
 };
